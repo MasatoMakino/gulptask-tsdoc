a4e2f6db7617eaaea99355baddf8f6ef12e8eca3
MasatoMakino <unagiinu.g@gmail.com>
2020-07-17 11:17:05 +0900

    update : bin.

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.get = void 0;
 const gulp_1 = require("gulp");
 const path_1 = __importDefault(require("path"));
 const gulp_replace_1 = __importDefault(require("gulp-replace"));
